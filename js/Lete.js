63979c5f9bd6bb522d3520027b98fd0d0c191820
kryane <2428907415@qq.com>
2023-03-25 20:27:35 +0800

    Site updated: 2023-03-25 20:27:34

--- js/Lete.js
+++ js/Lete.js
@@ -2,3 +2,12 @@
 $(document).ready(function(e){
     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
 })
+
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
+
+/*添加图片top*/
+var top_up = "<img id='upj' class='upj' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
