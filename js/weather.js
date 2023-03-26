fe31a7555ad174d322f6245b5ca6ca77b8554f44
kryane <2428907415@qq.com>
2023-03-27 03:42:53 +0800

    Site updated: 2023-03-27 03:42:52

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "borderRadius": "10",
+      "fixed": "true",
+      "vertical": "top",
+      "horizontal": "left",
+      "left": "260",
+      "top": "10",
+      "key": "cd909943da834f60b1ddc114d9b1deaf"
+    }
+  }
